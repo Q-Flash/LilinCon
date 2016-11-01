3015138caa75c46b77a71f2f706bbd2de92643b0
Mubarak Patel <mubarakdcricketer@hotmail.com>
2016-11-01 17:41:37 -0400

    App is running and added in HTML files

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+angular.module('starter.configs', [])
+
+.constant("CONFIG", {
+  "FIREBASE_API": 'AIzaSyBKVj26cCzX5CNJFXStUfdFavwAh4LbsWY',
+  "FIREBASE_AUTH_DOMAIN": 'lilincon-sports.firebaseapp.com',
+  "FIREBASE_DB_URL": 'https://lilincon-sports.firebaseio.com',
+  "FIREBASE_STORAGE": 'lilincon-sports.appspot.com',
+  "MESSAGE_SENDER_ID": '58433888402'
+});
